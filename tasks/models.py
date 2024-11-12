866afb6bcc862aef5d1a3a5bce3107150153e00a
hgpnguyen <phucgia1808@gmail.com>
2024-11-12 20:51:05 +0700

    add Task table

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICE = [
+        ('P', 'Pending'),
+        ('C', 'Completed')
+    ]
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICE)
