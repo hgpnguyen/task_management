4beacbbc3b711a85858f9f911aca274b8cd43293
hgpnguyen <phucgia1808@gmail.com>
2024-11-13 14:56:56 +0700

    add task view serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,12 +2,20 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(max_length=10)
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'status']
+        fields = ['title', 'description', 'status']
     
     def validate_status(self, value):
         value = value.lower().strip()
         if value not in ['p', 'c', 'pending', 'completed']:
             raise serializers.ValidationError('This is not a valid value for status')
         return value[0].upper()
+    
+    
+class TaskViewSerializer(serializers.ModelSerializer):
+    status = serializers.CharField(source='get_status_display')
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status']
